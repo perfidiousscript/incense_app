322da40ea50e60de54a977e55f790e356c9685aa
Sam Moss <sam.moss321@gmail.com>
2021-09-18 14:41:33 -0700

    adds methods to auth admit to ApplicationController

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
   include Clearance::Controller
+
+  def authenticate_admin_user!
+    redirect_to sign_in_path unless current_user && current_user.admin?
+  end
+
+  def current_admin_user
+    current_user
+  end
+  
 end
