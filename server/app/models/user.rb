37428a4558118bfb06bdd76d6f08e3e4c9923c44
Sam Moss <sam.moss321@gmail.com>
2021-09-18 14:15:40 -0700

    adds user model, adds clearance

---
+++ server/app/models/user.rb
@@ -0,0 +1,11 @@
+class User > ApplicationRecord
+  include Clearance::User
+
+  enum role: [:user, :moderator, :admin]
+
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :role, inclusion: { in: roles.keys, message: :invalid }
+end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_210022) do
+ActiveRecord::Schema.define(version: 2021_09_18_211139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -23,7 +23,11 @@ ActiveRecord::Schema.define(version: 2021_09_18_210022) do
     t.datetime "disabled_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_token"], name: "index_users_on_remember_token"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
