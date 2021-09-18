c079068738b33cdb3c9849517a951cf8f386e3e8
Sam Moss <sam.moss321@gmail.com>
2021-09-18 14:41:00 -0700

    adds users routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resource :users, only: %i[create update] do
+        get :current, on: :collection
+      end
+    end
+  end
 end
