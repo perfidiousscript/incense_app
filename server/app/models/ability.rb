a99dfc282c3fe7b0daeb77fd5628a293037f3d3c
Sam Moss <sam.moss321@gmail.com>
2021-09-19 16:32:04 -0700

    adds brand abilities

--- server/app/models/ability.rb
+++ server/app/models/ability.rb
@@ -4,9 +4,13 @@ class Ability
 
   def initialize(user)
     can [:current, :create], User
+    can [:show, :index], Brand
 
     if user.present?
       can [:show, :update], User, id: user.id
+      can [:create], Brand
+      if user.moderator?
+      end
       if user.admin?
         can :manage, :all
       end
