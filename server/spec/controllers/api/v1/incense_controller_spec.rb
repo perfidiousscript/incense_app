6b350c083e2ad1c19518fde18500e6a65a72cd51
Sam Moss <sam.moss321@gmail.com>
2021-09-21 11:26:44 -0700

    fleshes out more incense#create specs

--- server/spec/controllers/api/v1/incense_controller_spec.rb
+++ server/spec/controllers/api/v1/incense_controller_spec.rb
@@ -28,7 +28,68 @@ RSpec.describe Api::V1::IncensesController, type: :controller do
         expect(Incense.count).to eq 1
         expect(Incense.first.approved?).to eq false
       end
+
+      it 'should error with no brand' do
+        user = create(:user)
+        brand = create(:brand, :approved)
+        incense = build(:incense)
+
+        sign_in_as user
+        post :create, params: {
+          incense: {
+            name: incense.name,
+            description: incense.description,
+            image_url: incense.image_url,
+          }
+        }
+
+        assert_response :unprocessable_entity
+        expect(Incense.count).to eq 0
+      end
+
+      it 'should error if brand already exists' do
+        user = create(:user)
+        brand = create(:brand, :approved)
+        incense = create(:incense)
+
+        expect(Incense.count).to eq 1
+
+        sign_in_as user
+        post :create, params: {
+          incense: {
+            name: incense.name,
+            description: incense.description,
+            image_url: incense.image_url,
+            brand_id: brand.id
+          }
+        }
+
+        assert_response :unprocessable_entity
+        expect(Incense.count).to eq 1
+        expect(Incense.first.approved?).to eq false
+      end
+    end
+
+    describe 'as a moderator' do
+      it 'should create an approved incense'do
+        user = create(:user, :moderator)
+        brand = create(:brand, :approved)
+        incense = build(:incense)
+
+        sign_in_as user
+        post :create, params: {
+          incense: {
+            name: incense.name,
+            description: incense.description,
+            image_url: incense.image_url,
+            brand_id: brand.id
+          }
+        }
+
+        assert_response :created
+        expect(Incense.count).to eq 1
+        expect(Incense.first.approved?).to eq true
+      end
     end
   end
-
 end
