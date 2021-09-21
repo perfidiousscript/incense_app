b4540869e22b0800a8549cce96b02aed32dbc01f
Sam Moss <sam.moss321@gmail.com>
2021-09-21 08:59:32 -0700

    adds incense routes and abilities

--- server/app/models/ability.rb
+++ server/app/models/ability.rb
@@ -5,12 +5,15 @@ class Ability
   def initialize(user)
     can [:current, :create], User
     can [:show, :index], Brand
+    can [:show, :index], Incense
 
     if user.present?
       can [:show, :update], User, id: user.id
       can [:create], Brand
+      can [:create], Incense
       if user.moderator?
-        can [:approve], Brand
+        can [:approve, :update], Brand
+        can [:approve, :update], Incense
       end
       if user.admin?
         can :manage, :all
