7e7c95eb50956e9d6d8fa9ca1263520045115d1d
Sam Moss <sam.moss321@gmail.com>
2021-09-23 19:33:30 -0700

    adds Review model

--- server/app/models/IngredientClassification.rb
+++
@@ -1,4 +0,0 @@
-class IngredientClassification < ApplicationRecord
-  belongs_to :incense
-  belongs_to :ingredient
-end
---
+++ server/app/models/review.rb
@@ -0,0 +1,20 @@
+class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :incense
+  # has_many :notes
+
+  property_list = %i|sweet smokey woody ethereal savory fruity herbal spicy citrus floral|
+
+  property_list.each do |property|
+    validates property, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, if: property.present?
+  end
+
+
+  enum rating: {
+    hate: 0,
+    dislike: 1,
+    neutral: 2,
+    like: 3,
+    love: 4
+  }
+end
