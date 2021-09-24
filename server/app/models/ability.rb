597a313a40d32f47ea0d5402d49f1248706b2fda
Sam Moss <sam.moss321@gmail.com>
2021-09-23 19:38:35 -0700

    adds reviews abilities

--- server/app/models/ability.rb
+++ server/app/models/ability.rb
@@ -7,11 +7,13 @@ class Ability
     can [:show, :index], Brand
     can [:show, :index], Incense
     can [:show, :index], Ingredient
+    can [:show, :index], Review
 
     if user.present?
       can [:show, :update], User, id: user.id
       can [:create], Brand
       can [:create], Incense
+      can [:create, :update], Review, user_id: user.id
       if user.moderator?
         can [:approve, :update], Brand
         can [:approve, :update], Incense
