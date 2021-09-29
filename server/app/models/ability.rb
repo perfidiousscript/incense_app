2d1b2d6a7c189fa6f07f1fef8f5cc9a05882fafc
Sam Moss <sam.moss321@gmail.com>
2021-09-28 17:10:25 -0700

    adds review_vote abilities

--- server/app/models/ability.rb
+++ server/app/models/ability.rb
@@ -14,6 +14,7 @@ class Ability
       can [:create], Brand
       can [:create], Incense
       can [:create, :update], Review, user_id: user.id
+      can [:create, :update, :destroy], ReviewVote, 
       if user.moderator?
         can [:approve, :update], Brand
         can [:approve, :update], Incense
