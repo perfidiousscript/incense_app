a74774ac3bf52c0a327bb671a9ae63823c558c56
Sam Moss <sam.moss321@gmail.com>
2021-09-28 17:10:50 -0700

    adds ReviewVote model

---
+++ server/app/models/review_vote.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: review_votes
+#
+#  id         :uuid             not null, primary key
+#  vote_type  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  review_id  :uuid             not null
+#  user_id    :uuid             not null
+#
+# Indexes
+#
+#  index_review_votes_on_review_id  (review_id)
+#  index_review_votes_on_user_id    (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (review_id => reviews.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class ReviewVote < ApplicationRecord
+  belongs_to :user
+  belongs_to :review
+
+  enum vote_type: [:down, :up]
+
+  validates :review_id, :user_id, :vote_type presence: true
+  validates :vote_type, inclusion: { in: vote_type.keys, message: :invalid }
+end
