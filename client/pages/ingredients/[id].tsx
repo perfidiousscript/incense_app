984d53234ba267e357d5accc3b18846f4cc83652
Sam Moss <sam.moss321@gmail.com>
2021-09-30 13:10:16 -0700

    hooks ingredient#show up to api

---
+++ client/pages/ingredients/[id].tsx
@@ -0,0 +1,53 @@
+import { NextPageContext, NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import App from "components/App";
+import Head from "next/head";
+import Ingredients from "/lib/api/ingredients";
+import { useQuery } from "react-query";
+
+type InitialProps = {
+  ingredient: Ingredient;
+};
+
+const IngredientShow: NextPage<InitialProps> = ({ ingredient }) => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { isLoading, isError, data, error } = useQuery(
+    ["ingredient", id],
+    Ingredients.get
+  );
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error}</span>;
+  }
+
+  return (
+    <App>
+      <Head>
+        <title>Incense-Hermitage::Ingredients</title>
+      </Head>
+
+      <div>
+        <div>
+          <p>Ingredient</p>
+          <ul>
+            {
+              <div key={data.id}>
+                <p>{data.name}</p>
+                <p>{data.description}</p>
+              </div>
+            }
+          </ul>
+        </div>
+      </div>
+    </App>
+  );
+};
+
+export default IngredientShow;
