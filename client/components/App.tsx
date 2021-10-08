c8cc605c98c70204b6e49a768d5b4cc31cb990cb
Sam Moss <sam.moss321@gmail.com>
2021-10-08 11:24:57 -0700

    adds authCheck to App

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,11 +2,23 @@ import { FC } from "react";
 import { ThemeProvider } from "styled-components";
 
 import Theme from "styles/theme";
-import Nav from "components/Nav";
+import Link from "next/link";
 import Config from "lib/config";
+import { useAuth } from "lib/auth";
 
-const App: FC<{}> = ({ children }) => {
-  return <ThemeProvider theme={Theme}>{children}</ThemeProvider>;
+const App: FC<{}> = ({ children, authCheck }) => {
+  const { user } = useAuth();
+
+  if (authCheck && user === undefined) {
+    return (
+      <div className="centeredText">
+        Please <Link href="/sign-in">Sign In</Link> or{" "}
+        <Link href="sign-up">Sign Up</Link> to continue.
+      </div>
+    );
+  } else {
+    return <ThemeProvider theme={Theme}>{children}</ThemeProvider>;
+  }
 };
 
 export default App;
