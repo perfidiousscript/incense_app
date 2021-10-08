81d31944819b2013d5bce29d9e2279261f874a21
Sam Moss <sam.moss321@gmail.com>
2021-10-08 11:27:13 -0700

    adds styling and auth to brands create

--- client/pages/brands/create.tsx
+++ client/pages/brands/create.tsx
@@ -4,10 +4,12 @@ import App from "components/App";
 import Head from "next/head";
 import Link from "next/link";
 import Brands from "/lib/api/brands";
+import { useAuth } from "lib/auth";
 import { useMutation } from "react-query";
 import { styles } from "/styles/Brands.module.css";
 
 const BrandCreate: NextPage<{}> = () => {
+  const { user } = useAuth();
   const [name, setName] = useState("");
   const [country, setCountry] = useState("");
   const [description, setDescription] = useState("");
@@ -49,15 +51,15 @@ const BrandCreate: NextPage<{}> = () => {
       let errorDetail = error.detail;
 
       return (
-        <>
+        <div className="centeredText">
           <div>Error: {errorDetail}</div>
           {expandErrorReason(error.params)}
-        </>
+        </div>
       );
     } else if (createResult.isSuccess) {
       let { data } = createResult;
       return (
-        <div>
+        <div className="centeredText">
           <div>Success! {data.name} has been created</div>
           <div>
             See your new brand <Link href={`/brands/${data.slug}`}>Here</Link>
@@ -66,61 +68,63 @@ const BrandCreate: NextPage<{}> = () => {
       );
     } else {
       return (
-        <form
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            maxWidth: "500px",
-          }}
-          onSubmit={submit}
-        >
-          <label htmlFor="name">Brand Name</label>
-          <input
-            name="brand"
-            onChange={({ target: { value } }) => setName(value)}
-            type="text"
-            disabled={createResult.isLoading}
-            value={name}
-          />
-          <label htmlFor="country">Country</label>
-          <input
-            name="country"
-            onChange={({ target: { value } }) => setCountry(value)}
-            type="text"
-            disabled={createResult.isLoading}
-            value={country}
-          />
-          <label htmlFor="description">Description</label>
-          <textarea
-            name="description"
-            onChange={({ target: { value } }) => setDescription(value)}
-            type="text"
-            disabled={createResult.isLoading}
-            value={description}
-          />
-          <label htmlFor="imageUrl">Image</label>
-          <input
-            name="imageUrl"
-            onChange={({ target: { value } }) => setImageUrl(value)}
-            type="file"
-            accept="img/*"
-            disabled={createResult.isLoading}
-          />
-          <button type="submit" disabled={createResult.isLoading}>
-            Create
-          </button>
-        </form>
+        <div className="generalForm">
+          <form
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: "500px",
+            }}
+            onSubmit={submit}
+          >
+            <label htmlFor="name">Brand Name</label>
+            <input
+              name="brand"
+              onChange={({ target: { value } }) => setName(value)}
+              type="text"
+              disabled={createResult.isLoading}
+              value={name}
+            />
+            <label htmlFor="country">Country</label>
+            <input
+              name="country"
+              onChange={({ target: { value } }) => setCountry(value)}
+              type="text"
+              disabled={createResult.isLoading}
+              value={country}
+            />
+            <label htmlFor="description">Description</label>
+            <textarea
+              name="description"
+              onChange={({ target: { value } }) => setDescription(value)}
+              type="text"
+              disabled={createResult.isLoading}
+              value={description}
+            />
+            <label htmlFor="imageUrl">Image</label>
+            <input
+              name="imageUrl"
+              onChange={({ target: { value } }) => setImageUrl(value)}
+              type="file"
+              accept="img/*"
+              disabled={createResult.isLoading}
+            />
+            <button type="submit" disabled={createResult.isLoading}>
+              Create
+            </button>
+          </form>
+        </div>
       );
     }
   }
 
   return (
-    <App>
+    <App authCheck="true">
       <Head>
         <title>IH::Brand:Create</title>
       </Head>
       <div className="pageTitle">Create a New Brand</div>
-      <div className="centeredText">{createBrandBody()}</div>
+      {createBrandBody()}
     </App>
   );
 };
