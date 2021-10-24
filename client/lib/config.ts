48a6b41f7a79f44db4a180618511c226b303057c
Sam Moss <sam.moss321@gmail.com>
2021-10-24 13:17:59 -0700

    makes lib/config TS compliant

--- client/lib/config.ts
+++ client/lib/config.ts
@@ -1,13 +1,21 @@
-const config = {
+type ConfigKey =
+  | "DEVELOPMENT"
+  | "PRODUCTION"
+  | "API_BASE_HOST"
+  | "IMGUR_BASE_HOST"
+  | "IMGUR_CLIENT_ID";
+
+type ConfigType = Record<ConfigKey | string, boolean | string>;
+
+const config: ConfigType = {
   DEVELOPMENT: process.env.NODE_ENV === "development",
   PRODUCTION: process.env.NODE_ENV === "production",
-
   API_BASE_HOST: process.env.NEXT_PUBLIC_API_BASE_HOST as string,
   IMGUR_BASE_HOST: "https://api.imgur.com",
   IMGUR_CLIENT_ID: process.env.NEXT_PUBLIC_IMGUR_CLIENT_ID as string,
 };
 
-Object.keys(config).forEach((configKey) => {
+Object.keys(config).forEach((configKey: ConfigKey | string) => {
   if (config[configKey] === undefined)
     throw new ReferenceError(`Environment Variable (${configKey}) Not Found`);
 });
