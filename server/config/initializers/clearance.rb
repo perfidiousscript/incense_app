579e416c4421f1f6b6efd6f8eecbfee48c26f381
Sam Moss <sam.moss321@gmail.com>
2021-11-22 20:46:24 -0800

    adds env var for cookie domain

--- server/config/initializers/clearance.rb
+++ server/config/initializers/clearance.rb
@@ -2,4 +2,5 @@ Clearance.configure do |config|
   config.mailer_sender = "noreply@incense_app"
   config.rotate_csrf_on_sign_in = false
   config.routes = false
+  config.cookie_domain = ENV["Cookie_Domain"]
 end
