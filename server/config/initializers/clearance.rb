daeae6ef5d3710f44eb49c7024cb5a4b3cd3a97b
Sam Moss <sam.moss321@gmail.com>
2021-11-23 10:43:53 -0800

    capitalizes cookie domain in clearnace initializer

--- server/config/initializers/clearance.rb
+++ server/config/initializers/clearance.rb
@@ -2,5 +2,5 @@ Clearance.configure do |config|
   config.mailer_sender = "noreply@incense_app"
   config.rotate_csrf_on_sign_in = false
   config.routes = false
-  config.cookie_domain = ENV["Cookie_Domain"]
+  config.cookie_domain = ENV["COOKIE_DOMAIN"]
 end
