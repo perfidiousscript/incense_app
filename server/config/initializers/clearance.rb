e0066601658c4a97ec79eda02f5d8844b91a1df8
Sam Moss <sam.moss321@gmail.com>
2021-11-23 16:16:11 -0800

    remove cookie domain from clearance

--- server/config/initializers/clearance.rb
+++ server/config/initializers/clearance.rb
@@ -2,6 +2,6 @@ Clearance.configure do |config|
   config.mailer_sender = "noreply@incense_app"
   config.rotate_csrf_on_sign_in = false
   config.routes = false
-  config.cookie_domain = ENV["COOKIE_DOMAIN"]
+  # config.cookie_domain = ENV["COOKIE_DOMAIN"]
   config.secure_cookie = true
 end
